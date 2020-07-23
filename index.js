d3a33b42801b145e54610fa5cdeeeb519f147cbd
Alex <15ambm@queensu.ca>
2020-07-23 12:02:22 -0400

    - 3.2-6

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 
-const persons =
+app.use(express.json())
+
+let persons =
  [
     { name: 'Arto Hellas', number: '040-123456' , id:1},
     { name: 'Ada Lovelace', number: '39-44-5323523' ,id:2},
@@ -12,6 +14,61 @@ const persons =
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})  
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const newPersons = persons.filter(person => person.id !== id)
+    persons = newPersons
+    console.log(persons)
+    res.status(204).end()
+})
+
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has ${persons.length} people</p>
+                <p>${new Date()}</p>`)
+})
+
+const generateID = () => {
+    return Math.floor(Math.random() * 100000)
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error:'no name field'
+        })
+    } else if (!body.number ) {
+        return res.status(400).json({
+            error:'no number field'
+        })
+    } else if (persons.find(p => p.name === body.name)) {
+        return res.status(400).json({
+            error:'name must be unique'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateID()
+    }
+
+    persons = persons.concat(person)
+    res.json(persons)
+})
+
 
 const PORT = 3001
 app.listen(PORT, () => {

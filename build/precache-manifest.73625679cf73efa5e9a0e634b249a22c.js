a8af6cb666685b66d1bb1168b68cb1dd870c018d
Alex <15ambm@queensu.ca>
2020-07-30 13:48:55 -0400

    serving static frontend

---
+++ build/precache-manifest.73625679cf73efa5e9a0e634b249a22c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac641ac429d4832b0a36ca9002cb4b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4742979d1f1e30a08507",
+    "url": "/static/css/main.a8a920f6.chunk.css"
+  },
+  {
+    "revision": "d63449f0ad11db7c45f7",
+    "url": "/static/js/2.c640ce78.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c640ce78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4742979d1f1e30a08507",
+    "url": "/static/js/main.909f4fe9.chunk.js"
+  },
+  {
+    "revision": "b6a2b2066d6758e5fc57",
+    "url": "/static/js/runtime-main.166885fe.js"
+  }
+]);
